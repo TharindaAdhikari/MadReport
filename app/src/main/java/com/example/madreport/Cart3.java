5726f74d8be692b472eda85ee3288bdd0c2448f2
TTA <tharitash123@gmail.com>
2020-08-17 09:19:21 +0530

    updated

---
+++ app/src/main/java/com/example/madreport/Cart3.java
@@ -0,0 +1,14 @@
+package com.example.madreport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cart3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart3);
+    }
+}
